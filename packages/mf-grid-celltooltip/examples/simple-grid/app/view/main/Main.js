fa43ef1b5e0ba5821ef88c7be2143e1ca2e1e257
Steffen Ritter <steffen.ritter@mayflower.de>
2015-07-03 15:50:01 +0200

    added better example

--- packages/mf-grid-celltooltip/examples/simple-grid/app/view/main/Main.js
+++ packages/mf-grid-celltooltip/examples/simple-grid/app/view/main/Main.js
@@ -13,37 +13,51 @@ Ext.define('simpleGrid.view.main.Main', {
         type: 'fit'
     },
 
-    store: Ext.create('Ext.data.Store', {
-        storeId:'employeeStore',
-        fields:['name', 'seniority', 'department'],
-        data: {'employees':[
-            { "name": "Michael Scott",  "seniority": 7, "department": "Management" },
-            { "name": "Dwight Schrute", "seniority": 2, "department": "Sales" },
-            { "name": "Jim Halpert",    "seniority": 3, "department": "Sales" },
-            { "name": "Kevin Malone",   "seniority": 4, "department": "Accounting" },
-            { "name": "Angela Martin",  "seniority": 5, "department": "Accounting" }
-        ]},
-        proxy: {
-            type: 'memory',
-            reader: {
-                type: 'json',
-                rootProperty: 'employees'
-            }
-        }
-    }),
+    loremIpsumNote: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ' +
+        'ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et' +
+        'ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum ' +
+        'dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore ' +
+        'magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita' +
+        'kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
 
-    items: [{
-        xtype: 'grid',
-        title: 'Employees',
-        store: Ext.data.StoreManager.lookup('employeeStore'),
-        columns: [
-            { text: 'Name',     dataIndex: 'name' },
-            { text: 'Seniority', dataIndex: 'seniority' },
-            { text: 'Department', dataIndex: 'department' }
-        ],
-        features: [{ftype:'celltooltip'}],
-        width: 200,
-        height: 275,
-        renderTo: Ext.getBody()
-    }]
+    initComponent: function () {
+        Ext.create('Ext.data.Store', {
+            storeId:'employeeStore',
+            fields:['name', 'seniority', 'department', 'notes'],
+            data: {'employees':[
+                { "name": "Michael Scott",  "seniority": 7, "department": "Management", "notes": this.loremIpsumNote },
+                { "name": "Dwight Schrute", "seniority": 2, "department": "Sales", "notes": this.loremIpsumNote },
+                { "name": "Jim Halpert",    "seniority": 3, "department": "Sales", "notes": this.loremIpsumNote },
+                { "name": "Kevin Malone",   "seniority": 4, "department": "Accounting", "notes": this.loremIpsumNote },
+                { "name": "Angela Martin",  "seniority": 5, "department": "Accounting", "notes": this.loremIpsumNote }
+            ]},
+            proxy: {
+                type: 'memory',
+                reader: {
+                    type: 'json',
+                    rootProperty: 'employees'
+                }
+            }
+        });
+
+        Ext.apply(this, {
+            items: [{
+                xtype: 'grid',
+                title: 'Employees',
+                store: Ext.data.StoreManager.lookup('employeeStore'),
+                columns: [
+                    { text: 'Name',     dataIndex: 'name' },
+                    { text: 'Seniority', dataIndex: 'seniority' },
+                    { text: 'Department', dataIndex: 'department' },
+                    { text: 'Notes', dataIndex: 'notes' }
+                ],
+                features: [{ftype:'celltooltip'}],
+                width: 200,
+                height: 275,
+                renderTo: Ext.getBody()
+            }]
+        });
+
+        this.callParent();
+    }
 });
